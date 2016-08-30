c2269e7b285fa3061fe7b64e6ccf9107482a7399
Mat Lee <ml7@uw.edu>
2016-08-30 16:53:10 -0700

    Name tweak.

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -34,9 +34,7 @@ def test_couchDB_decorated_function_call_success():
     }
 
     decorated_foo = decorators.couchDBLogging(foo, couch_db_config)
-
     expected_return_value = '12'
-
     test_doc_id = str(uuid.uuid4())
 
     actual_return_value = decorated_foo(test_doc_id, 1, some_kwarg=2)
@@ -45,25 +43,25 @@ def test_couchDB_decorated_function_call_success():
     # Get value from test couchDB
     # TODO: find a better way to wait for write to CouchDB...
     time.sleep(.1)
-    write_response = requests.get(
+    write_result_response = requests.get(
         os.path.join(remote_host, test_db, test_doc_id)
     )
-    write_response_object = json.loads(write_response.text)
+    write_result_response_object = json.loads(write_result_response.text)
 
-    logging.info(write_response.text)
+    logging.info(write_result_response.text)
 
-    assert(ast.literal_eval(write_response_object['args'])[0] == 1)
-    assert(ast.literal_eval(write_response_object['kwargs'])['some_kwarg'] == 2)
-    assert(write_response_object['start_time'] is not None)
-    assert(write_response_object['end_time'] is not None)
+    assert(ast.literal_eval(write_result_response_object['args'])[0] == 1)
+    assert(ast.literal_eval(write_result_response_object['kwargs'])['some_kwarg'] == 2)
+    assert(write_result_response_object['start_time'] is not None)
+    assert(write_result_response_object['end_time'] is not None)
 
-    logging.info( os.path.join(remote_host, test_db, test_doc_id + '?rev={}'.format(write_response_object['_rev'])) )
+    logging.info( os.path.join(remote_host, test_db, test_doc_id + '?rev={}'.format(write_result_response_object['_rev'])) )
 
     # Clean up
     delete_response = requests.delete(
-        os.path.join(remote_host, test_db, test_doc_id + '?rev={}'.format(write_response_object['_rev']))
+        os.path.join(remote_host, test_db, test_doc_id + '?rev={}'.format(write_result_response_object['_rev']))
     )
 
-    logging.info( write_response_object['_rev'] )
+    logging.info( write_result_response_object['_rev'] )
 
     assert(delete_response.status_code == 200)
